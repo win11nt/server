1de12c43d83cb7afff5cc896270103a22fcb1846
Minh <achillesback0511@yahoo.com>
2023-07-25 19:30:25 +0700

    asd

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(cors());
 app.use(function (req, res, next) {
 
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'http://192.168.1.54:3000');
+  res.setHeader('Access-Control-Allow-Origin', 'https://combative-purse-foal.cyclic.app');
 
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
