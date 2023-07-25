77df02a3f5628390ebc59248f1c3a8b86ed2dd2a
Minh <achillesback0511@yahoo.com>
2023-07-25 19:18:20 +0700

    hhi

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(cors());
 app.use(function (req, res, next) {
 
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'http://192.168.1.54:3000');
 
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
